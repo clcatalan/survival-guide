dfe931fa6bd16fc0a8524023027c022857a257f1
kennvash <39698876+kennvash@users.noreply.github.com>
2019-08-02 20:16:25 +0800

    made calendar zoomable on tap

---
+++ app/src/main/java/com/example/survivalguide/Calendar_Full_1.java
@@ -0,0 +1,26 @@
+package com.example.survivalguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoViewAttacher;
+
+public class Calendar_Full_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar__full_1);
+        getSupportActionBar().hide();
+
+        ImageView imgview = findViewById(R.id.calendar_full1);
+        Drawable bitmap = getResources().getDrawable(R.drawable.uplb_academic_calendar1);
+        imgview.setImageDrawable(bitmap);
+
+        PhotoViewAttacher imgAttacher = new PhotoViewAttacher(imgview);
+        imgAttacher.update();
+    }
+}
---
+++ app/src/main/java/com/example/survivalguide/Calendar_Full_2.java
@@ -0,0 +1,26 @@
+package com.example.survivalguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoViewAttacher;
+
+public class Calendar_Full_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar__full_2);
+        getSupportActionBar().hide();
+
+        ImageView imgview = findViewById(R.id.calendar_full2);
+        Drawable bitmap = getResources().getDrawable(R.drawable.uplb_academic_calendar2);
+        imgview.setImageDrawable(bitmap);
+
+        PhotoViewAttacher imgAttacher = new PhotoViewAttacher(imgview);
+        imgAttacher.update();
+    }
+}
